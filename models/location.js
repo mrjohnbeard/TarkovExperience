495a7f48566a09b8c345f1126a1e8967db80ac22
mrjohnbeard <wengerbeard@gmail.com>
2021-12-17 14:03:39 -0800

    add location model

---
+++ models/location.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const locationSchema = new Schema({
+
+});
+
+
+module.exports = mongoose.model('Location', locationSchema);
