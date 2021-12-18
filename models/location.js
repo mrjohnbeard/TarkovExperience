2e9a40c6d45328f57855f60a029c2a70857e57d6
mrjohnbeard <wengerbeard@gmail.com>
2021-12-17 19:17:20 -0800

    location models updated

--- models/location.js
+++ models/location.js
@@ -7,25 +7,28 @@ const locationSchema = new Schema(
 
         area: {
             type: String,
-            enum: ['Customs', 'Woods'],
+            enum: ['Customs', 'Factory', 'Interchange', 'LightHouse', 'Reserve', 'Shoreline', 'The Lab', 'Woods'],
+        },
+        
+        duration: {
+            type: Number,
+        },
+        
+        difficulty: {
+            type: String,
+            enum: ['Easy', 'Hard', 'Expert', 'Insane'],
         },
 
+        enemies: {
+            type: String,
+            enum: [ 'Cultists', 'Glukhar', 'Killa',  'Raiders', 'Rouges', 'Sanitar', 'Scavs', 'Shturman', 'Tagila', 'Reshala'],
+        },
+        
         players: {
             type: Number,
             min: 5,
             max: 20,
         },
-
-        duration: {
-            type: Number,
-        },
-
-        difficulty: {
-            type: String,
-            enum: ['Easy', 'Hard'],
-        },
-
-
 
     },
     {
