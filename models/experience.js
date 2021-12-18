309e41b630e4ce28b03a5e6e16d7f2095561925f
mrjohnbeard <wengerbeard@gmail.com>
2021-12-18 02:53:39 -0800

    add show ejs

--- models/experience.js
+++ models/experience.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const experienceSchema = new Schema({
+
+    kill: {
+        type: Number,
+        min: 0
+    },
   
     location: {
         type: Schema.Types.ObjectId,
