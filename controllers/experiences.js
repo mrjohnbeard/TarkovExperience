4eb3f3511b06bebab168927b7f0433a1b58c8790
mrjohnbeard <wengerbeard@gmail.com>
2021-12-18 03:14:11 -0800

    functions complete

--- controllers/experiences.js
+++ controllers/experiences.js
@@ -8,13 +8,14 @@ const Experience = require('./../models/experience');
 function newExperience(req, res) {
     res.render('experiences/new', {
         title: `Add Experience`,
+        locationNo: req.params.id
     })
 };
 
 function create(req, res) {
     req.body.location = req.params.id;
     Experience.create(req.body, function (err) {
-        res.redirect(`/experiences/${req.params.id}`);
+        res.redirect(`/locations/${req.params.id}`);
     });
 };
 
