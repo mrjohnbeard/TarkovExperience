648e8600ac84e6ddfe6eeab734e1875502fe948a
mrjohnbeard <wengerbeard@gmail.com>
2021-12-18 03:21:04 -0800

    add username to experience

--- models/experience.js
+++ models/experience.js
@@ -7,11 +7,19 @@ const experienceSchema = new Schema({
         type: Number,
         min: 0
     },
-  
+
     location: {
         type: Schema.Types.ObjectId,
         ref: 'Location'
-    }
-});
+    },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    userName: String,
+    userAvatar: String
+},
+
+
+    {
+        timestamps: true
+    });
 
 module.exports = mongoose.model('Experience', experienceSchema);
