5a933b08df03d9e2637f82cdcc32523b9fb0dfc9
mrjohnbeard <wengerbeard@gmail.com>
2021-12-17 16:33:04 -0800

    location Schema base

--- models/location.js
+++ models/location.js
@@ -1,9 +1,37 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const locationSchema = new Schema({
+const locationSchema = new Schema(
+    {
 
-});
+
+        area: {
+            type: String,
+            enum: ['Customs', 'Woods'],
+        },
+
+        players: {
+            type: Number,
+            min: 5,
+            max: 20,
+        },
+
+        duration: {
+            type: Number,
+        },
+
+        difficulty: {
+            type: String,
+            enum: ['Easy', 'Hard'],
+        },
+
+
+
+    },
+    {
+        timestamps: true
+    }
+);
 
 
 module.exports = mongoose.model('Location', locationSchema);
