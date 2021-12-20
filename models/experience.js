a558e1fdfbbb4f4658d559d1a957dc46380566f2
mrjohnbeard <wengerbeard@gmail.com>
2021-12-20 14:52:25 -0800

    add properties 2 experience model

--- models/experience.js
+++ models/experience.js
@@ -1,21 +1,67 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const experienceSchema = new Schema({
+const experienceSchema = new Schema(
+    {
 
-    kill: {
-        type: Number,
-        min: 0
-    },
+        kill:
+        {
+            type: Number,
+            min: 0,
+        },
 
-    location: {
-        type: Schema.Types.ObjectId,
-        ref: 'Location'
+        travel:
+        {
+            type: Number,
+            min: 0,
+        },
+
+        damageBody:
+        {
+            type: Number,
+            min: 0,
+        },
+
+        damageArmor:
+        {
+            type: Number,
+            min: 0,
+        },
+
+        ammo:
+        {
+            type: Number,
+            min: 0,
+        },
+
+        hitCount:
+        {
+            type: Number,
+            min: 0
+        },
+
+        hpHealed:
+        {
+            type: Number,
+            min: 0
+        },
+
+        hpUsed:
+        {
+            type: Number,
+            min: 0
+        },
+
+        location: {
+            type: Schema.Types.ObjectId,
+            ref: 'Location'
+        },
+
+
+        user: { type: Schema.Types.ObjectId, ref: 'User' },
+        userName: String,
+        userAvatar: String
     },
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
-    userName: String,
-    userAvatar: String
-},
 
 
     {
